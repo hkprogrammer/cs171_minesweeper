a6e7840b293ac1d0deec1f3861b736d460ab950a
Jason Zenarosa <jasonzenarosa@gmail.com>
2023-05-14 15:56:10 -0700

    minimal AI

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -14,73 +14,211 @@
 
 from AI import AI
 from Action import Action
-import random
 
 
 class MyAI( AI ):
 
+
 	def __init__(self, rowDimension, colDimension, totalMines, startX, startY):
 		self.rowDimension = rowDimension
 		self.colDimension = colDimension
-		self.board = [[None]*colDimension]*rowDimension
 		self.totalMines = totalMines
-		self.coveredTiles = colDimension*rowDimension
 		self.currentX = startX
 		self.currentY = startY
-		self.currentNumber = 0
-  
-		#assume uncovered at starX and startY
-	
 
-	def updateBoard(self, x, y, value) -> None:
-		if x < self.rowDimension and y < self.colDimension:
-			self.board[x][y] = value
-			self.coveredTiles-=1
+		self.coveredTiles = rowDimension * colDimension
+		self.board = [[None for j in range(colDimension)] for i in range(rowDimension)]
 		
-	def checkEndCondition(self) ->bool:
-		if self.coveredTiles == self.totalMines:
-			return True
-		#and other losing conditions
-		return False
-
-	def moveCondition(self,new_x,new_y) -> bool:
-		return not (0<=new_x < self.colDimension and 0<=new_y <self.rowDimension) or (self.board[new_y][new_x] == None)
 
 	def getAction(self, number: int) -> "Action Object":
-		
-		
-		if number == 0:
-			# move anywhere in 8 directions in random
-			new_x,new_y = -1,-1
-			while self.moveCondition(new_x, new_y):
-				direction = random.randint(0,4)
-				new_x,new_y = self.currentX,self.currentY
-				if direction == 0:
-					#move right
-					new_x +=1
-				elif direction == 1: 
-					#move up
-					new_y -= 1
-				elif direction == 2:
-					#move left
-					new_x -= 1
-				else:
-					#move down
-					new_y += 1
-		
-		self.updateBoard(new_x,new_y,number)
-  
-  
-		self.currentX,self.currentY = new_x, new_y
-		return Action(AI.Action.UNCOVER, new_x, new_y)
-		
-	
-  
-		return Action(AI.Action.LEAVE)
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
+		self.updateBoard(self.currentX, self.currentY, number)
+		# for i in self.board:
+		# 	print(i)
+		# print(self.coveredTiles)
+		# print(self.currentX, self.currentY)
+		# print()
 
+		if self.coveredTiles == self.totalMines:
+			return Action(AI.Action.LEAVE)
+		if number == 0:
+			self.getActionZero()
+		if number == 1:
+			self.getActionOne()
+
+		return Action(AI.Action.UNCOVER, self.currentX, self.currentY)
+	
+
+	def getActionZero(self):
+		if self.isValidMove(self.currentX + 1, self.currentY):
+			self.currentX += 1
+		elif self.isValidMove(self.currentX, self.currentY + 1):
+			self.currentY += 1
+		elif self.isValidMove(self.currentX - 1, self.currentY):
+			self.currentX -= 1
+		elif self.isValidMove(self.currentX, self.currentY - 1):
+			self.currentY -= 1
+		else:
+			self.scan()
+
+
+	def getActionOne(self):
+		# if tile above or below is a 0
+		if (self.inBounds(self.currentX, self.currentY + 1) and self.board[self.currentX][self.currentY + 1] == 0) or (self.inBounds(self.currentX, self.currentY - 1) and self.board[self.currentX][self.currentY - 1] == 0):
+			# move right
+			if self.isValidMove(self.currentX + 1, self.currentY):
+				self.currentX += 1
+			# move left
+			elif self.isValidMove(self.currentX - 1, self.currentY):
+				self.currentX -= 1
+			# if no valid move, scan
+			else:
+				self.scan()
+
+		# if tile right or left is a 0
+		elif (self.inBounds(self.currentX + 1, self.currentY) and self.board[self.currentX + 1][self.currentY] == 0) or (self.inBounds(self.currentX - 1, self.currentY) and self.board[self.currentX - 1][self.currentY] == 0):
+			# move down
+			if self.isValidMove(self.currentX, self.currentY + 1):
+				self.currentY += 1
+			# move up
+			elif self.isValidMove(self.currentX, self.currentY - 1):
+				self.currentY -= 1
+			# if no valid move, scan
+			else:
+				self.scan()
+
+		# if tile below is a 1
+		elif (self.inBounds(self.currentX, self.currentY + 1) and self.board[self.currentX][self.currentY + 1] == 1):
+			# move up
+			if self.isValidMove(self.currentX, self.currentY - 1):
+				self.currentY -= 1
+			# if no valid move, scan
+			else:
+				self.scan()
+
+		# if tile above is a 1
+		elif (self.inBounds(self.currentX, self.currentY - 1) and self.board[self.currentX][self.currentY - 1] == 1):
+			# move down
+			if self.isValidMove(self.currentX, self.currentY + 1):
+				self.currentY += 1
+			# if no valid move, scan
+			else:
+				self.scan()
+
+		# if tile right is a 1
+		elif (self.inBounds(self.currentX + 1, self.currentY) and self.board[self.currentX + 1][self.currentY] == 1):
+			# move left
+			if self.isValidMove(self.currentX - 1, self.currentY):
+				self.currentX -= 1
+			# if no valid move, scan
+			else:
+				self.scan()
+
+		# if tile left is a 1
+		elif (self.inBounds(self.currentX - 1, self.currentY) and self.board[self.currentX - 1][self.currentY] == 1):
+			# move right
+			if self.isValidMove(self.currentX + 1, self.currentY):
+				self.currentX += 1
+			# if no valid move, scan
+			else:
+				self.scan()
+
+		else:
+			self.scan()
+
+
+	def scan(self):
+		# Scan for a covered tile with an adjacent zero
+		for x in range(self.colDimension):
+			for y in range(self.rowDimension):
+				if self.board[x][y] is None and self.hasAdjacentZero(x, y):
+					self.currentX = x
+					self.currentY = y
+					return
+		
+		# Scan for three in a row
+		for x in range(self.colDimension - 2):
+			for y in range(self.rowDimension):
+				# if both to the right of a 1 are also 1s
+				if self.board[x][y] == 1 and self.board[x+1][y] == 1 and self.board[x+2][y] == 1:
+					if self.isValidMove(x, y - 1):
+						self.currentX = x
+						self.currentY = y - 1
+						return
+					elif self.isValidMove(x, y + 1):
+						self.currentX = x
+						self.currentY = y + 1
+						return
+					elif self.isValidMove(x + 2, y - 1):
+						self.currentX = x + 2
+						self.currentY = y - 1
+						return
+					elif self.isValidMove(x + 2, y + 1):
+						self.currentX = x + 2
+						self.currentY = y + 1
+						return
+
+		for x in range(self.colDimension):
+			for y in range(self.rowDimension - 2):
+				if self.board[x][y] == 1 and self.board[x][y+1] == 1 and self.board[x][y+2] == 1:
+					if self.isValidMove(x - 1, y):
+						self.currentX = x - 1
+						self.currentY = y
+						return
+					elif self.isValidMove(x + 1, y):
+						self.currentX = x + 1
+						self.currentY = y
+						return
+					elif self.isValidMove(x - 1, y + 2):
+						self.currentX = x - 1
+						self.currentY = y + 2
+						return
+					elif self.isValidMove(x + 1, y + 2):
+						self.currentX = x + 1
+						self.currentY = y + 2
+						return
+
+		# If no specific pattern is found, select the first covered tile
+		for x in range(self.colDimension):
+			for y in range(self.rowDimension):
+				if self.board[x][y] is None:
+					self.currentX = x
+					self.currentY = y
+					return
+
+
+	def updateBoard(self, x, y, val):
+		if self.board[x][y] == None:
+			self.board[x][y] = val
+			self.coveredTiles -= 1
+	
+
+	def inBounds(self, x, y):
+		if x < 0 or x >= self.colDimension:
+			return False
+		if y < 0 or y >= self.rowDimension:
+			return False
+		return True
+
+
+	def isValidMove(self, x, y):
+		if not self.inBounds(x, y):
+			return False
+		if self.board[x][y] != None:
+			return False
+		return True
+
+	def hasAdjacentZero(self, x, y):
+		adjacentTiles = [
+			(x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
+			(x - 1, y),                 (x + 1, y),
+			(x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
+		]
+
+		for adjX, adjY in adjacentTiles:
+			if self.inBounds(adjX, adjY) and self.board[adjX][adjY] == 0:
+				return True
+		
+		return False
 
 if __name__ == "__main__":
-    MyAI(5,5,10,0,0)
+	MyAI(5,5,1,0,0)
